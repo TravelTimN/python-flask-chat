794b2e34e6a4942d813d79cdac60add966625964
Tim Nelson <traveltimn@gmail.com>
2019-01-20 10:47:49 +0000

    implemented basic views for username and message

--- run.py
+++ run.py
@@ -5,7 +5,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello World!</h1>"
+    # index page with instructions
+    return "To send a message, use /USERNAME/MESSAGE"
+
+@app.route("/<username>")
+def user(username):
+    return "Hi <b>" + username.capitalize()
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username.capitalize(), message.upper())
 
 app.run(host=os.getenv("IP"),
         port=os.getenv("PORT"),
